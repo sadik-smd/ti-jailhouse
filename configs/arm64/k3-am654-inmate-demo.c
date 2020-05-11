6b079933875fc4561871b2d51fb969bd685c5c18
Nikhil Devshatwar <nikhil.nd@ti.com>
2020-05-11 20:19:32 +0530

    configs: k3-am654-inmate-demo: Add ivshmem capability
    Add a virtual PCI device with IVSHMEM type (id = 1)
    Create IVSHMEM regions for 3 peer communication
    Enable the vpci_irq for doorbell interrupt
    
    This allows to run the ivshmem-demo baremetal inmate
    inside this cell.
    
    Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

--- configs/arm64/k3-am654-inmate-demo.c
+++ configs/arm64/k3-am654-inmate-demo.c
@@ -20,7 +20,9 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[3];
+	struct jailhouse_memory mem_regions[8];
+	struct jailhouse_irqchip irqchips[1];
+	struct jailhouse_pci_device pci_devices[1];
 } __attribute__((packed)) config = {
 	.cell = {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
@@ -30,8 +32,9 @@ struct {
 
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
-		.num_irqchips = 0,
-		.num_pci_devices = 0,
+		.num_irqchips = 1,
+		.num_pci_devices = 1,
+		.vpci_irq_base = 189 - 32,
 
 		.console = {
 			.address = 0x40a00000,
@@ -47,6 +50,39 @@ struct {
 	},
 
 	.mem_regions = {
+		/* IVSHMEM shared memory regions for 00:00.0 (demo) */
+		{
+			.phys_start = 0x8dfb00000,
+			.virt_start = 0x8dfb00000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		{
+			.phys_start = 0x8dfb10000,
+			.virt_start = 0x8dfb10000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED |
+				 JAILHOUSE_MEM_WRITE ,
+		},
+		{
+			.phys_start = 0x8dfb20000,
+			.virt_start = 0x8dfb20000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		{
+			.phys_start = 0x8dfb30000,
+			.virt_start = 0x8dfb30000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED |
+				 JAILHOUSE_MEM_WRITE ,
+		},
+		{
+			.phys_start = 0x8dfb40000,
+			.virt_start = 0x8dfb40000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_ROOTSHARED,
+		},
 		/* MCU UART0 */ {
 			.phys_start = 0x40a00000,
 			.virt_start = 0x40a00000,
@@ -67,5 +103,27 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_COMM_REGION,
 		},
-	}
+	},
+
+	.irqchips = {
+		{
+			.address = 0x01800000,
+			.pin_base = 160,
+			.pin_bitmap = {
+				1 << (189 - 160), 0x0, 0x00, 0,
+			},
+		},
+	},
+
+	.pci_devices = {
+		/* 00:00.0 */ {
+			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+			.bdf = 0 << 3,
+			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
+			.shmem_regions_start = 0,
+			.shmem_dev_id = 1,
+			.shmem_peers = 3,
+			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+		},
+	},
 };
