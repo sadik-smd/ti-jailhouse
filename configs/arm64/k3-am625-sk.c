e85c739164a80bb896c1425c0ef7a6eee700b218
Andreas Dannenberg <dannenberg@ti.com>
2023-11-27 14:27:37 +0530

    configs: arm64: k3-am625-sk: Add GPMC memory region
    Add the memory region for GPMC, used by the AM62Q NAND card.
    
    Upstream-Status: Submitted [jailhouse-dev@googlegroups.com]
    
    Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
    Signed-off-by: Paresh Bhagat <p-bhagat@ti.com>

--- configs/arm64/k3-am625-sk.c
+++ configs/arm64/k3-am625-sk.c
@@ -18,7 +18,7 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[33];
+	struct jailhouse_memory mem_regions[35];
 	struct jailhouse_irqchip irqchips[5];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
@@ -229,6 +229,20 @@ struct {
 			.phys_start = 0x31100000,
 			.virt_start = 0x31100000,
 			.size = 0x00050000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* GPMC */ {
+			.phys_start = 0x3b000000,
+			.virt_start = 0x3b000000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* GPMC */ {
+			.phys_start = 0x50000000,
+			.virt_start = 0x50000000,
+			.size = 0x08000000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
